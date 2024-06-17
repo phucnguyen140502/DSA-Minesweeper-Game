5c6edd1f0cb22d9e594e1b56fae9e1d85a9f50f6
phucnguyen140502 <nguyenducnguyenphuc@gmail.com>
2024-06-17 13:00:57 +0700

    update

--- Minesweeper/src/view/Game.java
+++ Minesweeper/src/view/Game.java
@@ -5,7 +5,6 @@ import ui.LoadAudio;
 public class Game {
     public static void main(String[] args) {
         LoadAudio.LoadData();
-//        LoadAudio.playStart();
         new GameFrame(9,9,10);
 
     }
